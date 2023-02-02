a0c71e4b3185c98d457c962e7928ff40cd33e5a7
Victor Hugo Prado Gomes <vitorvex@gmail.com>
2023-02-02 13:49:45 -0300

    Add files via upload

---
+++ For.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Course
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros vão ser somados?");
+            int times = int.Parse(Console.ReadLine());
+            int add = 0;
+
+            for (int count = 1; times >= count; count++)
+            {
+               Console.Write($"Valor #{count}:");
+               int teste = int.Parse(Console.ReadLine());
+               add += teste;
+            }
+            Console.Write($"Resultado da soma dos valores:{add}");
+        }
+    }
+}
